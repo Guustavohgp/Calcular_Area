11878c144ccf2991a59e746ae5f207d4029af032
Gustavohgp <gustavo.henriquegpires@gmail.com>
2024-06-07 10:10:14 -0300

    creat class trapezio

---
+++ entities/Trapezio.java
@@ -0,0 +1,48 @@
+package Calcular_Area.entities;
+
+public class Trapezio extends Formas {
+
+    private Double basemaior ;
+    private Double basemenor;
+    private Double altura;
+
+    public Trapezio() {
+        super();
+    }
+
+    public Trapezio(Double basemaior, Double basemenor, Double altura) {
+        this.basemaior = basemaior;
+        this.basemenor = basemenor;
+        this.altura = altura;
+    }
+
+    public Double getBasemaior() {
+        return basemaior;
+    }
+
+    public void setBasemaior(Double basemaior) {
+        this.basemaior = basemaior;
+    }
+
+    public Double getBasemenor() {
+        return basemenor;
+    }
+
+    public void setBasemenor(Double basemenor) {
+        this.basemenor = basemenor;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public double area() {
+    return (basemaior+basemenor)*altura /2 ;
+    }
+
+}
