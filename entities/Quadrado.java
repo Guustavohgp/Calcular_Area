feb342614f6a9b079274b6244f4056d5269c0860
Gustavohgp <gustavo.henriquegpires@gmail.com>
2024-06-07 09:52:17 -0300

    create class quadrado

--- entities/Quadrado.java
+++ entities/Quadrado.java
@@ -1,5 +1,26 @@
 package Calcular_Area.entities;
 
-public class Quadrado {
+public class Quadrado extends Formas{
+    private Double lado;
+
+    public Quadrado(){
+        super();
+    }
     
+    public Quadrado(Double lado) {
+        this.lado = lado;
+    }
+    
+    public Double getLado() {
+        return lado;
+    }
+
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double area() {
+        return lado*lado;
+    }   
 }
