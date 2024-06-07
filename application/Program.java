3354c6cc0fafd1d1596a3fe0003f640a527ff723
Gustavohgp <gustavo.henriquegpires@gmail.com>
2024-06-07 10:31:06 -0300

    add case cir

--- application/Program.java
+++ application/Program.java
@@ -1,5 +1,45 @@
 package Calcular_Area.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Digite o número de formas: ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("Forma #" + i + ":");
+            System.out.println("Escolha a forma");
+            System.out.print("Círculo (cir), Retângulo (ret), Quadrado (qua), Triângulo (tri) e Trapézio (tra)");
+            String sigla = sc.nextLine();
+            
+            switch (sigla) {
+                case "cir":
+                System.out.println("Raio:");
+                double raio = sc.nextDouble();
+                    
+                break;
+                case "ret":
+                    
+                break;
+                case "qua":
+                    
+                break;
+                case "tri":
+                    
+                break;
+                case "tra":
+                    
+                break;
+            
+                default:
+                    break;
+            }
+        }
+    }
     
 }
