1edb6118ba20fafc22cda6b1efc29358476d4c29
Gustavohgp <gustavo.henriquegpires@gmail.com>
2024-06-07 09:48:40 -0300

    create class circulo

--- entities/Circulo.java
+++ entities/Circulo.java
@@ -1,5 +1,26 @@
 package Calcular_Area.entities;
 
-public class Circulo {
+public class Circulo extends Formas{
+    private Double raio;
+
+    public Circulo(){
+        super();
+    }
     
+    public Circulo(Double raio) {
+        this.raio = raio;
+    } 
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+    
+    @Override
+    public double area() {
+        return Math.PI * raio *raio;
+    }  
 }
