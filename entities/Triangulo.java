07344071d281b4a3f476c896696c27331b5461b4
Gustavohgp <gustavo.henriquegpires@gmail.com>
2024-06-07 10:03:52 -0300

    creat class triangulo

--- entities/Triangulo.java
+++ entities/Triangulo.java
@@ -1,5 +1,38 @@
 package Calcular_Area.entities;
 
-public class Triangulo {
+public class Triangulo extends Formas {
+    private Double altura;
+    private Double largura;
+
+    public Triangulo() {
+        super();
+    }
+
+    public Triangulo(Double altura, Double largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(Double largura) {
+        this.largura = largura;
+    }
+
+    @Override
+    public double area() {
+       return (largura * altura) /2 ;
+    }
+
     
 }
